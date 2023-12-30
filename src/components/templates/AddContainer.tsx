09c0539ef1167d102030d90dc22218188c806a41
m0r3a <buretdan52@gmail.com>
2023-12-31 00:16:03 +0200

    changed styling at cta

--- src/components/templates/AddContainer.tsx
+++ src/components/templates/AddContainer.tsx
@@ -75,7 +75,7 @@ const AddContainer: React.FC<AddContainerProps> = ({ heightProp, widthProp }) =>
             <img src="${imageUrlWithParams}" alt="Generated Image" style="width: 100%; height: 100%; object-fit: cover;"/>
             <h2 style="position: absolute; top: 5%; left: 50%; transform: translateX(-50%); color: white; width: 100%; text-align: center; text-shadow: -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black;">${title}</h2>
             <p style="position: absolute; top: 25%; left: 50%; transform: translate(-50%, -50%); color: white; width: 100%; text-align: center; text-shadow: -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black;">${description}</p>
-            <button style="position: absolute; bottom: 20px; left: 50%; transform: translateX(-50%); color: white; background: transparent; min-width: 60px; max-width: calc(100% - 100px); height: 50px; padding: 0 20px; border: 1px solid white;">${cta}</button>
+            <button style="position: absolute; bottom: 20px; left: 50%; transform: translateX(-50%); color: white; background: transparent; min-width: 60px; max-width: calc(100% - 100px); height: 50px; padding: 0 20px; border: 1px solid white; text-shadow: -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black; box-shadow: -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black;">${cta}</button>
           </div>
         `;
       }
