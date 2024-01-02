9f9ee43d80de0d6d3f48d4917376aaea7a2b761e
m0r3a <buretdan52@gmail.com>
2024-01-02 05:41:06 +0200

    Addded styled components in AddContainer.css

--- src/components/templates/AddContainer.tsx
+++ src/components/templates/AddContainer.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react';
 import domtoimage from 'dom-to-image';
-import './Styles/AddContainer.css';
+import styled from 'styled-components';
 import AddManager from './AddManager';
 import openAi from 'openai';
 
@@ -9,18 +9,117 @@ const openai = new openAi({
   dangerouslyAllowBrowser: true,
 });
 
-
 interface AddContainerProps {
   heightProp: string;
   widthProp: string;
 }
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  box-sizing: border-box;
+  height: 90vh;
+  position: relative;
+  top: 2.5vh;
+`;
+
+const Element = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ImageContainer = styled.div<{ imageUrl: string, height: string, width: string }>`
+  background-color: transparent;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  height: ${(props) => props.height};
+  width: ${(props) => props.width};
+  background-image: ${(props) => `url(${props.imageUrl})`};
+  background-size: cover;
+  background-position: center;
+
+  h2 {
+    position: relative;
+    top: 10%;
+    text-shadow: -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black;
+    text-align: center;
+  }
+
+  p {
+    position: relative;
+    top: -15%;
+    margin: 0 15px;
+    text-align: center;
+    text-shadow: -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black;
+  }
+
+  button {
+    position: relative;
+    bottom: 10%;
+    background: transparent;
+    min-width: 60px;
+    max-width: calc(100% - 100px);
+    height: 50px;
+    padding: 0 20px;
+    border: 1px solid white;
+    box-shadow: -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black;
+    color: white;
+    text-shadow: -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black;
+  }
+`;
+
+const DownloadButton = styled.button`
+  position: relative;
+  top: 5%;
+  padding: 16px;
+  background: linear-gradient(0deg, #fcfcfc 0%, #f5f5f5 100%);
+  border: 6px solid transparent;
+  border-radius: 16px;
+  background-clip: padding-box;
+  box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.2), 0 1px 4px 0 rgba(0, 0, 0, 0.5), 1px 3px 3px 2px #fff,
+    0 0 0 6px #ECECEC;
+  border-radius: 16px;
+  font-family: "Inter", sans-serif;
+  font-size: 16px;
+  line-height: 1;
+  transition: border 0.1s ease-in-out, padding 0.1s ease-in-out, margin 0.1s ease-in-out;
+
+  &:after {
+    position: absolute;
+    top: -6px;
+    bottom: -6px;
+    left: -6px;
+    right: -6px;
+    background: linear-gradient(#fff, #ededed);
+    content: "";
+    z-index: -1;
+    border-radius: 16px;
+  }
+
+  &:hover {
+    border: 2px solid transparent;
+    background-image: linear-gradient(180deg, #E6E6E6 0%, #F7F7F7 100%);
+    box-shadow: 0 0 2px 0 rgba(0, 0, 0, 0.35), 0 0 0 6px #ECECEC;
+    padding: 20px;
+  }
+
+  &:active {
+    border-top: 1px solid transparent;
+  }
+`;
 
 const AddContainer: React.FC<AddContainerProps> = ({ heightProp, widthProp }) => {
   const imageContainerRef = useRef<HTMLDivElement>(null);
   const [title, setTitle] = useState<string>('Title');
   const [description, setDescription] = useState<string>('Short description');
   const [cta, setCta] = useState<string>('Call to Action button');
-  const [generatedImageUrl, setGeneratedImageUrl] = useState<string>('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiqo_V7sqPhLLvsU4Q-z1E52dYR6clW06TsrrkjE6wGeVn_oSeff0yK4nEOcBQGLFmbSk&usqp=CAU');
+  const [generatedImageUrl, setGeneratedImageUrl] = useState<string>(
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiqo_V7sqPhLLvsU4Q-z1E52dYR6clW06TsrrkjE6wGeVn_oSeff0yK4nEOcBQGLFmbSk&usqp=CAU'
+  );
 
   const handleChangeGeneration = async (title: string, description: string, cta: string) => {
     setTitle(title);
@@ -66,10 +165,8 @@ const AddContainer: React.FC<AddContainerProps> = ({ heightProp, widthProp }) =>
     if (generatedImageUrl) {
       const imageTab = window.open('', '_blank');
       if (imageTab) {
-        // Construct the image URL with query parameters
         const imageUrlWithParams = `${generatedImageUrl}`;
-  
-        // Set the window content to a div with the image and overlay text
+
         imageTab.document.body.innerHTML = `
           <div style="position: relative; width: ${widthProp}; height: ${heightProp}; margin: 0 auto; text-align: center; overflow: hidden;">
             <img src="${imageUrlWithParams}" alt="Generated Image" style="width: 100%; height: 100%; object-fit: cover;"/>
@@ -82,47 +179,42 @@ const AddContainer: React.FC<AddContainerProps> = ({ heightProp, widthProp }) =>
     }
   };
 
-  const handleApplyTittle = (newTitle: string) => {
+  const handleApplyTitle = (newTitle: string) => {
     setTitle(newTitle);
-  }
+  };
 
   const handleApplyDesc = (newDesc: string) => {
     setDescription(newDesc);
-  }
+  };
 
   const handleApplyCta = (newCta: string) => {
     setCta(newCta);
-  }
-  
+  };
+
   return (
-    <div className="add-container">
-      <div className="first-elem">
-        <div
-          className="image-container"
-          style={{
-            height: heightProp,
-            width: widthProp,
-            backgroundImage: `url(${generatedImageUrl})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
+    <Container>
+      <Element>
+        <ImageContainer
+          height={heightProp}
+          width={widthProp}
+          imageUrl={generatedImageUrl}
           ref={imageContainerRef}
         >
           <h2>{title}</h2>
           <p>{description}</p>
           <button onClick={handleOpenImageInNewTab}>{cta}</button>
-        </div>
-        <button className="button-27" onClick={handleDownloadClick} role="button">
+        </ImageContainer>
+        <DownloadButton onClick={handleDownloadClick} role="button">
           <span className="text">Download</span>
-        </button>
-      </div>
+        </DownloadButton>
+      </Element>
       <AddManager
         generateValues={handleChangeGeneration}
-        changeTitle={handleApplyTittle}
+        changeTitle={handleApplyTitle}
         changeDesc={handleApplyDesc}
         changeCta={handleApplyCta}
       />
-    </div>
+    </Container>
   );
 };
 
