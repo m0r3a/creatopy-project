8462e8645b8ab940c73db96d0ba2734169f5f05c
m0r3a <buretdan52@gmail.com>
2024-01-02 05:24:06 +0200

    Implemented Header with styled-components

--- src/components/templates/Header.tsx
+++ src/components/templates/Header.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
-import './Styles/Header.css';
+import styled from 'styled-components';
 
 interface HeaderProps {
-  name: string
+  name: string;
 }
 
-const Header: React.FC<HeaderProps> = ({name}) => {
-  
+const HeaderComponent = styled.div`
+  background-color: white;
+  width: calc(100% - 10px); /* for padding 10px */
+  height: 45px;
+  color: black;
+  font-size: 24px;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  padding-left: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
+
+const NameSize = styled.div`
+  position: absolute;
+  right: 10px;
+  font-size: 16px;
+`;
+
+const Header: React.FC<HeaderProps> = ({ name }) => {
   return (
-    <div className="header">
-      AI Ad Generator 
-      <div className='name'>{name}</div>
-    </div>
+    <HeaderComponent>
+      AI Ad Generator
+      <NameSize>{name}</NameSize>
+    </HeaderComponent>
   );
 };
 
